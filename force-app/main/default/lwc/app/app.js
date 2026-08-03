6a6de847e0f274fc0067b99441ee6c3399684c79
Willard Soriano <124260532+willardcsoriano@users.noreply.github.com>
2026-08-03 16:46:56 +0800

    feat(unit-02): add bikeCard and app LWC components (#1)

---
+++ force-app/main/default/lwc/app/app.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+export default class App extends LightningElement {
+  name = 'Electra X4';
+  description = 'A sweet bike built for comfort.';
+  category = 'Mountain';
+  material = 'Steel';
+  price = '$2,700';
+  pictureUrl = 'https://s3-us-west-2.amazonaws.com/dev-or-devrl-s3-bucket/sample-apps/ebikes/electrax4.jpg';
+  ready = false;
+  connectedCallback() {
+    setTimeout(() => {
+      this.ready = true;
+    }, 3000);
+  }
+}
